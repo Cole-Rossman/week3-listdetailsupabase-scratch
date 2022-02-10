301f59e0071b4923e90570779b36ed7a1334e331
Cole Rossman <colerossman99@gmail.com>
2022-02-09 18:11:42 -0800

    renderCharacter function and test complete and work

--- render-utils.js
+++ render-utils.js
@@ -2,21 +2,51 @@ export function renderCharacters(character) {
     const div = document.createElement('div');
     const a = document.createElement('a');
     const img = document.createElement('img');
-    const p = document.createElement('p');
+    const nameP = document.createElement('p');
 
     div.classList.add('character-select');
     
     img.src = `/assets/${character.image}.png`;
     img.classList.add('character-image');
 
-    p.textContent = character.name;
-    p.classList.add('character-name');
+    nameP.textContent = character.name;
+    nameP.classList.add('character-name');
 
 
     a.href = `./character-page/?id=${character.id}`;
 
-    div.append(img, p);
+    div.append(img, nameP);
     a.append(div);
 
     return a;
+}
+
+export function renderCharacter(character) {
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const nameP = document.createElement('p');
+    const quoteP = document.createElement('p');
+    const occupationP = document.createElement('p');
+    const likesP = document.createElement('p');
+
+    div.classList.add('character-select');
+    
+    img.src = `/assets/${character.image}.png`;
+    img.classList.add('character-image');
+
+    nameP.textContent = character.name;
+    nameP.classList.add('character-name');
+
+    quoteP.textContent = character.quote;
+    quoteP.classList.add('character-quote');
+
+    occupationP.textContent = character.occupation;
+    occupationP.classList.add('character-occupation');
+
+    likesP.textContent = character.likes;
+    likesP.classList.add('character-likes');
+
+    div.append(img, nameP, quoteP, occupationP, likesP);
+    return div;
+
 }
--- test/render-utils.test.js
+++ test/render-utils.test.js
@@ -1,4 +1,4 @@
-import { renderCharacters } from '../render-utils.js';
+import { renderCharacters, renderCharacter } from '../render-utils.js';
 
 const test = QUnit.test;
 
@@ -17,4 +17,21 @@ test('renderCharacters should create a div displaying character image, name and 
 
     expect.equal(actual.outerHTML, expected);
 
+});
+
+test('renderCharacter should create a div and display all of the information about the character from supabase', (expect) => {
+    const expected = `<div class="character-select"><img src="/assets/pleakley.png" class="character-image"><p class="character-name">Wendy Pleakley</p><p class="character-quote">Notable Quote: "HERE! Educate yourself."</p><p class="character-occupation">Occupation: United Galactic Federation Agent</p><p class="character-likes">Agent Pleakley enjoys the following: Cleanliness, mosquitos and disguises.</p></div>`;
+
+    
+    const agentPleakley = {
+        id: '4',
+        name: 'Wendy Pleakley',
+        quote: 'Notable Quote: "HERE! Educate yourself."',
+        occupation: 'Occupation: United Galactic Federation Agent',
+        likes: 'Agent Pleakley enjoys the following: Cleanliness, mosquitos and disguises.',
+        image: 'pleakley',
+    };
+    const actual = renderCharacter(agentPleakley);
+
+    expect.equal(actual.outerHTML, expected);
 });
